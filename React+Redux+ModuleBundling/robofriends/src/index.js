971eb010a1dad58dbf8c401f684c09a006c96479
gleyconxavier <gleyconxcarlos@gmail.com>
2020-02-25 17:10:45 -0400

    Updates

--- React+Redux+ModuleBundling/robofriends/src/index.js
+++ React+Redux+ModuleBundling/robofriends/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CardList from './components/CardList';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 import "tachyons";
-import {robots} from "./robots";
 
 ReactDOM.render(
-<CardList robots={robots} />
+<App />
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
