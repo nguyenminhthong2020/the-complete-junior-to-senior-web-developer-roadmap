4c1d8131707eaeb24394d450d28bf560e47014f5
gleyconxavier <gleyconxcarlos@gmail.com>
2020-03-02 23:09:44 -0400

    Updates | Snapshot testing

---
+++ Testing/Robofriends-PWA/src/components/Card.test.js
@@ -0,0 +1,7 @@
+import {shallow, mount, reder} from 'enzyme';
+import React from 'react';
+import Card from './Card';
+
+it('expect to render Card component', () => {
+    expect(shallow(<Card />)).toMatchSnapshot();
+})
